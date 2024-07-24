a42cc2531c630c745cdd834a0ad19deead6cc344
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2024-07-24 19:32:52 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+class sample extends Frame
+       {
+public static void main(String[] args)
+             {
+     // creating objact of frame
+     sample fr = new sample();
+     fr.setVisible(true);
+     fr.setSize(300,300);
+     fr.setTitle("welcome to arrow academy");
+                } 
+      }
