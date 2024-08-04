13343dd971a1b07fff99ca415b29c1d6743b9e09
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2024-08-04 17:58:17 +0530

    Add files via upload

---
+++ binarytreelevel.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+    
+         List<List<Integer>> result = new ArrayList<>();
+
+         if(root == null) return result;
+         Queue<TreeNode> queue = new LinkedList<>();
+
+         queue.add(root);
+
+         while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> listLevel = new ArrayList<>();
+
+            for(int i = 0; i < size; i++){
+             TreeNode node = queue.poll();
+              listLevel.add(node.val);
+
+              if(node.left != null){
+                queue.add(node.left);
+              }
+
+              if(node.right != null){
+                queue.add(node.right);
+              }
+            }
+              
+              result.add(listLevel);
+         }
+           return result;
+    }
+}
