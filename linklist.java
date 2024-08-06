2e5b8da1b4ecae07056f514386542a13274fcc02
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2024-08-06 18:49:06 +0530

    Add files via upload

---
+++ linklist.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class linklist
+{
+public static void main(String args[])
+{
+//Node next;
+LinkedList<String> ll= new LinkedList<String>();
+ll.add("A");
+ll.add("B");
+ll.addFirst("1");
+ll.addFirst("0");
+ll.addLast("C");
+
+System.out.println(ll);
+
+}
+}
