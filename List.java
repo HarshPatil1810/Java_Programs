a4962b137a0c0c204d677c858f478c2f49757118
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2024-08-08 18:28:56 +0530

    Add files via upload

---
+++ List.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+//import java.applet.*;
+public class EX33 
+{
+EX33()
+{
+Frame f=new Frame();
+f.setSize(300,300);
+f.setVisible(true);
+//Choice ch=new Choice();
+List l=new List();
+l.setBounds(100,100,75,75);
+l.add("c");
+l.add("c++");
+l.add("c++");
+l.add("c++");
+l.add("c++");
+l.add("c++");
+//ch.add("Summer");
+//ch.add("Summer1");
+//ch.add("Summer2");
+f.add(l);
+//f.add(ch);
+}
+public static void main(String args[])
+{
+EX33 e=new EX33();
+}
+}
+
