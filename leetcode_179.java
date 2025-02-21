ea59a10497da7999cc5941e498e4db7aaffabcb5
Harshvardhan Patil <152369369+HarshPatil1810@users.noreply.github.com>
2025-02-21 10:53:27 +0530

    Create leetcode_179.java

---
+++ leetcode_179.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        int n=nums.length;
+        String[] s=new String[n];
+
+        for(int i=0;i<n;i++){
+            s[i]=String.valueOf(nums[i]);
+        }
+        Arrays.sort(s,(a,b)->(b+a).compareTo(a+b));
+
+        StringBuilder sb=new StringBuilder();
+        for(String str:s){
+            sb.append(str);
+        }
+
+        String res=sb.toString();
+        return res.startsWith("0") ? "0" : res;
+        
+        
+    }
+}
